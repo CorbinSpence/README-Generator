9232328f4cd501321f4417187fc5b6b29ba82040
CorbinSpence <corbinspence16@gmail.com>
2023-03-24 16:20:28 -0400

    Altered installations

---
+++ README.md
@@ -0,0 +1,1 @@
+# README Generater
--- index.js
+++ index.js
@@ -1,54 +1,64 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
-
-inquirer.prompt([
+async function runPrompt(){
+await inquirer.prompt([
     {
         type: "input",
         name: "title",
-        message: "What is the project's title?"
+        message: "What is the project's title?",
     },
     {
         type: "input",
         name: "description",
-        message: "Type a description for the project."
+        message: "Type a description for the project.",
     },
     {
         type: "input",
         name: "installation",
-        message: "Explain what you should install and how to install it."
+        message: "Explain what you should install and how to install it.",
     },
     {
         type: "input",
         name: "usage",
-        message: "Explain how you should use this program?"
+        message: "Explain how you should use this program?",
     },
     {
         type: "input",
         name: "contributing",
-        message: "List out project contributor's"
+        message: "List out project contributor's",
     },
     {
         type: "input",
         name: "testing",
-        message: "Explain how to test this program."
+        message: "Explain how to test this program.",
     },
     {
         type: "input",
         name: "license",
-        message: "Choose you lisensing."
+        message: "Choose you lisensing.",
     },
     {
         type: "input",
         name: "github",
-        message: "What is your github URL?"
+        message: "What is your github URL?",
     },
     {
         type: "input",
         name: "email",
-        message: "What is your email."
+        message: "What is your email.",
     },
 ]).then(displayData(response))
-
-var displayData = (response) => {
-
 }
+var displayData = (response) => {
+    console.log("starting data list")
+    console.log(response.title)
+    console.log(response.description)
+    console.log(response.installation)
+    console.log(response.usage)
+    console.log(response.contributing)
+    console.log(response.testing)
+    console.log(response.license)
+    console.log(response.github)
+    console.log(response.email)
+}
+runPrompt()
