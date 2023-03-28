5932e74149035c4949e015b533b158662d86bf8f
CorbinSpence <corbinspence16@gmail.com>
2023-03-28 13:31:07 -0400

    fixed formating

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-
 # README Generator
+
+![License](https://img.shields.io/badge/license-None-lightgrey)
 
 ## Description
 
 This program uses javascript and node.js to create a specified README file. The user runs index.js with node.js to initiate a series of prompts. After the prompts have been filled, a README will be created based on the provided parameters.
+
+## Table of Contents
+
+1. [Installation](#Installation)
+2. [Usage](#Usage)
+3. [License](#License)
+4. [Contributing](#Contributing)
+5. [Tests](#Tests)
+6. [Questions](#Questions)
 
 ## Installation
 
@@ -13,18 +23,20 @@ You should have Node.js ver 16.0.0 to run the program.
 
 This program should be used to streamline the process of creating READMEs for individual projects.
 
-## Contributors
+## License
+
+None
+
+## Contributing
 
 Corbin Spence
 
-## Testing
+## Tests
 
 Open your console, change directory so that you are in the folder where index.js is, and type "node index.js" to run. prompts should follow afterwards.
 
-## License
+## Questions
 
-N/A
+Github: [Profile](https://github.com/CorbinSpence)
 
-## Github Profile
-
-https://github.com/CorbinSpence
+Email: corbinspence16@gmail.com
--- index.js
+++ index.js
@@ -1,51 +1,62 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+
+var licenses = ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3.0", "None"]
+var badges = {}
+
+badges["MIT"] = 'https://img.shields.io/badge/license-MIT-yellowgreen'
+badges["APACHE 2.0"] = 'https://img.shields.io/badge/license-Apache-blue'
+badges["GPL 3.0"] = 'https://img.shields.io/badge/license-GPL%203.0-blue'
+badges["BSD 3.0"] = 'https://img.shields.io/badge/license-BSD%203.0-yellowgreen'
+badges["None"] = 'https://img.shields.io/badge/license-None-lightgrey'
+
 async function runPrompt(){
 await inquirer.prompt([
     {
         type: "input",
         name: "title",
-        message: "What is the project's title?",
+        message: "What is the project's title?:",
     },
     {
         type: "input",
         name: "description",
-        message: "Type a description for the project.",
+        message: "Type a description for the project:",
     },
     {
         type: "input",
         name: "installation",
-        message: "Explain what you should install and how to install it.",
+        message: "Explain what you should install and how to install it:",
     },
     {
         type: "input",
         name: "usage",
-        message: "Explain how you should use this program?",
+        message: "Explain how you should use this program:",
     },
     {
         type: "input",
         name: "contributing",
-        message: "List out project contributor's",
+        message: "List out project contributor's:",
     },
     {
         type: "input",
         name: "testing",
-        message: "Explain how to test this program.",
+        message: "Explain how to test this program:",
     },
     {
-        type: "input",
+        type: "list",
         name: "license",
-        message: "Choose you lisensing.",
+        message: "Choose you lisensing:",
+        choices: [...licenses],
     },
     {
         type: "input",
         name: "github",
-        message: "What is your github URL?",
+        message: "What is your github URL?:",
     },
     {
         type: "input",
         name: "email",
-        message: "What is your email.",
+        message: "What is your email?:",
     },
 ]).then((response)=>{
     console.log("starting data list")
@@ -61,9 +72,20 @@ await inquirer.prompt([
     let content = 
 `# ${response.title}
 
+![License](${badges[response.license]})
+
 ## Description
 
 ${response.description}
+
+## Table of Contents
+
+1. [Installation](#Installation)
+2. [Usage](#Usage)
+3. [License](#License)
+4. [Contributing](#Contributing)
+5. [Tests](#Tests)
+6. [Questions](#Questions)
 
 ## Installation
 
@@ -73,25 +95,23 @@ ${response.installation}
 
 ${response.usage}
 
-## Contributors
-
-${response.contributing}
-
-## Testing
-
-${response.testing}
-
 ## License
 
 ${response.license}
 
-## Github Profile
+## Contributing
 
-${response.github}
+${response.contributing}
 
-## Contact
+## Tests
 
-${response.email}
+${response.testing}
+
+## Questions
+
+Github: [profile](${response.github})
+
+Email: ${response.email}
 `;
     fs.writeFile('README.md', content, (err) => err? console.log('failure') : console.log('success'))
 })
